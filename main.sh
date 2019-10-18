ab9a07e1aa9a266bc28897d3e05e94d0f647060a
Kshitij Gupta <kshitijgm@gmail.com>
2019-10-18 14:51:34 +0530

    Add pubkeys and setup-script
    - Made for c2.medium.x86

--- main.sh
+++ main.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-echo "ohai!"
+# root setup script is installing byobu, parted, git and zsh
+
+# Add users, and copy public keys
+BASEDIR=$(dirname "$0")
+for user in $(ls $BASEDIR/keys); do
+  useradd -m $user
+  usermod -aG sudo $user 
+  cp -R $BASEDIR/keys/$user/* /home/$user/
+  echo "chown -R $user:$user /home/$user";
+done
+sed -i "s/^%sudo/# %sudo/" /etc/sudoers
+echo "%sudo ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+# Initialize disks
+parted /dev/sdc --script -- mklabel gpt mkpart primary 0% 100%
+parted /dev/sdd --script -- mklabel gpt mkpart primary 0% 100%
+mkdir -p /volumes/v1
+mkdir -p /volumes/v2
+mount /dev/sdc1 /volumes/v1
+mount /dev/sdd1 /volumes/v2
+chown -R jenkins:jenkins /volumes
