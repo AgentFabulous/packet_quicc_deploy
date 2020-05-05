fb183d2ad22475ce7779b1a46ae5607218af9bf2
Kshitij Gupta <kshitijgm@gmail.com>
2020-05-05 17:08:04 +0530

    cloudflare-update: Always attempt to fetch ids

--- cloudflare-update.sh
+++ cloudflare-update.sh
@@ -6,7 +6,6 @@ record_name="$2.$3"
 proxied="$4"
 
 ip=$(ip addr | grep bond0 | grep inet | awk '{$1=$1};1' | cut -d \  -f 2 | cut -d \/ -f 1 | head -n 1)
-id_file="cloudflare.ids"
 
 if ! [[ "$ip" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
     message="Fetched IP does not look valid! Quitting"
@@ -14,15 +13,8 @@ if ! [[ "$ip" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
     exit 1
 fi
 
-if [ -f $id_file ] && [ $(wc -l $id_file | cut -d " " -f 1) == 2 ]; then
-    zone_identifier=$(head -1 $id_file)
-    record_identifier=$(tail -1 $id_file)
-else
-    zone_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone_name" -H "Authorization: Bearer $api_token" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
-    record_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" -H "Authorization: Bearer $api_token" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*')
-    echo "$zone_identifier" > $id_file
-    echo "$record_identifier" >> $id_file
-fi
+zone_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone_name" -H "Authorization: Bearer $api_token" -H "Content-Type: application/json" | grep -Po '(?<="id":")[^"]*' | head -1 )
+record_identifier=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone_identifier/dns_records?name=$record_name" -H "Authorization: Bearer $api_token" -H "Content-Type: application/json"  | grep -Po '(?<="id":")[^"]*')
 
 echo "zone : $zone_identifier"
 echo "record : $record_identifier"
